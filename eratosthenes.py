9e70d6e5b9bb26f4a05fd43c49100d889fa7aff5
Kiriko-I <kirikotrombone@gmail.com>
2023-06-21 21:20:28 +0900

    [Add]eratosthenes.py

---
+++ eratosthenes.py
@@ -0,0 +1,17 @@
+N = int(input())
+
+is_prime = ["YES"] * (N+1)
+is_prime[0], is_prime[1] = "NO", "NO"
+
+sq = int(N**0.5)
+
+for i in range(sq+1):
+    if is_prime[i] == "NO":
+        continue # すでにFalseの場合は次の数字の処理をするために先頭に戻る
+    for j in range(i**2, N+1, i):
+        is_prime[j] = "NO"
+primes = []
+for i, j in enumerate(is_prime): # なじみがないのであとでrangeに直してみる
+    if j == "YES":
+        primes.append(i)
+print(primes)
