ce2132d20772330d8e9647660712fbc26c58c336
Kiriko-I <kirikotrombone@gmail.com>
2023-06-22 18:44:03 +0900

    [Add]eratosthenes.py修正

--- eratosthenes.py
+++ eratosthenes.py
@@ -6,12 +6,29 @@ is_prime[0], is_prime[1] = "NO", "NO"
 sq = int(N**0.5)
 
 for i in range(sq+1):
-    if is_prime[i] == "NO":
-        continue # すでにNOの場合は次の数字の処理をするために先頭に戻る
-    for j in range(i**2, N+1, i):
-        is_prime[j] = "NO"
+    if is_prime[i] == "YES":
+        for j in range(i**2, N+1, i):
+            is_prime[j] = "NO"
+            
 primes = []
 for i, j in enumerate(is_prime): # なじみがないのでいつかrangeに直してみる
     if j == "YES":
         primes.append(i)
 print(primes)
+
+# その数が素数かどうかを判定するプログラムの回答例
+
+# is_prime = [True] * (N + 1)
+
+# is_prime[0] = False
+# is_prime[1] = False
+
+# for i in range(2, N + 1):
+#     if is_prime[i]:
+#         for j in range(i * 2, N + 1, i):
+#             is_prime[j] = False
+
+# if is_prime[N]:
+#     print("YES")
+# else:
+#     print("NO")
